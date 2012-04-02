f0d9abce13708dedc8d340b0da1e3aa64f86cf4b
Tim Huegdon <tim@nefariousdesigns.co.uk>
2012-04-02 14:45:53 +0100

    Adding more manifest goodness including bash WIN.

--- manifests/base.pp
+++ manifests/base.pp
@@ -4,5 +4,57 @@ import "nginx.pp"
 stage { "pre": before => Stage["main"] }
 stage { "last": require => Stage["main"] }
 
+class devbox {
+    exec { "aptitude update --quiet --assume-yes":
+        alias => "aptupdate",
+        path => "/usr/bin",
+        user => "root",
+        timeout => 0,
+        before => Package["build-essential"]
+    }
+    user { "vagrant":
+        groups => [
+            "sudo"
+        ]
+    }
+    group { "puppet":
+        ensure => present,
+    }
+    package { "build-essential":
+        ensure => latest,
+        before => Package["vim"],
+    }
+    package { [
+            "python-software-properties",
+            "vim",
+            "aptitude"
+        ]:
+        ensure => latest,
+    }
+    package { "memcached":
+        ensure => latest
+    }
+    file { "/home/vagrant/.bashrc":
+        owner  => vagrant,
+        group  => vagrant,
+        mode   => 644,
+        source => "puppet:////vagrant/manifests/files/bashrc",
+    }
+    file { "/home/vagrant/.bash_aliases":
+        owner  => vagrant,
+        group  => vagrant,
+        mode   => 644,
+        source => "puppet:////vagrant/manifests/files/bash_aliases",
+    }
+    file { "/etc/motd":
+        owner  => root,
+        group  => root,
+        mode   => 644,
+        source => "puppet:////vagrant/manifests/files/motd",
+    }
+}
+
+class { "devbox": stage => pre }
+
 include python
 include nginx
